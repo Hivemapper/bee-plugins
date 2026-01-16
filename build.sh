1650c0a243e7b7571bb11b473bab24b59a5dc373
evanmoss <evan@hivemapper.com>
2026-01-16 02:11:06 -0800

    swap order

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ENTRY_POINT="${1:-plugin:main}"
-PLUGIN_NAME="${2:-myplugin.py}"
+PLUGIN_NAME="${1:-myplugin.py}"
+ENTRY_POINT="${2:-plugin:main}"
 
 python3 -m zipapp src -m $ENTRY_POINT -p "/usr/bin/python3" -o $PLUGIN_NAME
