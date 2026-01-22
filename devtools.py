9c9dfdfa78bf0e162c7443f6a0e9bae3fe8fe6f6
evanmoss <evan@hivemapper.com>
2026-01-21 16:41:53 -0800

    add wifi tools

--- devtools.py
+++ devtools.py
@@ -6,7 +6,10 @@ from requests.exceptions import HTTPError
 from scp import SCPClient
 
 HOST_IP = '192.168.0.10'
-DEVICE_PLUGIN_ROUTE = f'http://{HOST_IP}:5000/api/1/plugin/'
+API_ROUTE = f'http://{HOST_IP}:5000/api/1'
+WIFI_ROUTE = f'http://{HOST_IP}:5000/wifiClient/settings'
+CONNECTIVITY_ROUTE = f'{API_ROUTE}/config/uploadMode'
+DEVICE_PLUGIN_ROUTE = f'{API_ROUTE}/plugin/'
 PAUSE_UPDATES_ROUTE = DEVICE_PLUGIN_ROUTE + 'setPausePluginUpdates'
 
 TEMPLATE_PLUGIN_PATH = '/data/plugins/template-plugin/template-plugin'
@@ -60,6 +63,49 @@ def restart_template_plugin_service():
 
     run_command_over_ssh(ssh, 'systemctl restart template-plugin')
 
+def toggle_client_connectivity_mode(mode):
+  res = requests.post(CONNECTIVITY_ROUTE, json={'mode': mode})
+
+  try:
+    res.raise_for_status()
+  except HTTPError as http_err:
+    try:
+      print(res.json())
+    except Exception:
+      pass
+
+    raise
+
+  return res.json()
+
+def switch_to_lte_client_mode():
+  toggle_client_connectivity_mode('lte')
+
+def switch_to_wifi_client_mode():
+  toggle_client_connectivity_mode('wifi')
+
+def connect_to_wifi_network(ssid, password, security='WPA2', freq=2417):
+  config = {
+    'ssid': ssid,
+    'password': password,
+    'enabled': 'true',
+    'security': security,
+    'freq': freq,
+  }
+  res = requests.post(WIFI_ROUTE, json=config)
+
+  try:
+    res.raise_for_status()
+  except HTTPError as http_err:
+    try:
+      print(res.json())
+    except Exception:
+      pass
+
+    raise
+
+  return res.json()  
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Local dev tooling for Bee Plugin development.")
 
@@ -67,6 +113,9 @@ if __name__ == '__main__':
   parser.add_argument('-dO', '--disable_devmode', help="Disable devmode.", action='store_true')
   parser.add_argument('-i', '--input_file', help="Path to build.sh output .py file.", type=str)
   parser.add_argument('-R', '--restart_plugin', help="Restart plugin", action='store_true')
+  parser.add_argument('-L', '--lte', help="Use LTE for connectivity")
+  parser.add_argument('-W', '--wifi_ssid', help="Use WiFi SSID for connectivity", type=str)
+  parser.add_argument('-P', '--password', help="Password", type=str, default="")
 
   args = parser.parse_args()
 
@@ -88,3 +137,9 @@ if __name__ == '__main__':
   if args.disable_devmode:
     disable_dev_mode()
     print('Dev Mode disabled')
+
+  if args.lte:
+    switch_to_lte_client_mode()
+  elif args.wifi_ssid:
+    switch_to_lte_client_mode()
+    connect_to_wifi_network(args.wifi_ssid, args.password)    
