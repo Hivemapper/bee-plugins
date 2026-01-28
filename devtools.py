4ed66a3b071eefb1473cf77e1efdacb237138f13
evanmoss <evan@hivemapper.com>
2026-01-27 16:29:50 -0800

    add cache helpers

--- devtools.py
+++ devtools.py
@@ -1,5 +1,6 @@
 # fmt: off
 import argparse
+import os
 import paramiko
 
 from util import do_json_post
@@ -12,6 +13,8 @@ DEVICE_PLUGIN_ROUTE = f'{API_ROUTE}/plugin/'
 PAUSE_UPDATES_ROUTE = DEVICE_PLUGIN_ROUTE + 'setPausePluginUpdates'
 
 TEMPLATE_PLUGIN_PATH = '/data/plugins/template-plugin/template-plugin'
+
+CACHE_DIR = '/data/cache';
 
 def run_command_over_ssh(ssh, cmd):
   stdin, stdout, stderr = ssh.exec_command(cmd)
@@ -51,6 +54,22 @@ def restart_template_plugin_service():
 
     run_command_over_ssh(ssh, 'systemctl restart template-plugin')
 
+def populate_fixture(fixture):
+  with paramiko.SSHClient() as ssh:
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(HOST_IP, username='root', password="", look_for_keys=False, allow_agent=False)
+
+    with SCPClient(ssh.get_transport()) as scp:
+      scp.put(f'fixtures/{fixture}', recursive=True, remote_path=CACHE_DIR)
+
+def dump_cache():
+  with paramiko.SSHClient() as ssh:
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(HOST_IP, username='root', password="", look_for_keys=False, allow_agent=False)
+
+    with SCPClient(ssh.get_transport()) as scp:
+      scp.get(CACHE_DIR, recursive=True)
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Local dev tooling for Bee Plugin development.")
 
@@ -58,6 +77,8 @@ if __name__ == '__main__':
   parser.add_argument('-dO', '--resume_plugin_updates', help="Resume plugin updates.", action='store_true')
   parser.add_argument('-i', '--input_file', help="Path to build.sh output .py file.", type=str)
   parser.add_argument('-R', '--restart_plugin', help="Restart plugin", action='store_true')
+  parser.add_argument('-f', '--populate_fixture', help="Populate fixture data", type=str)
+  parser.add_argument('-d', '--dump_cache', help='Copy cache contents to local machine', action='store_true')
 
   args = parser.parse_args()
 
@@ -79,3 +100,9 @@ if __name__ == '__main__':
   if args.resume_plugin_updates:
     resume_plugin_updates()
     print('Plugin updates are resumed.')
+
+  if args.populate_fixture:
+    populate_fixture(args.populate_fixture)
+
+  if args.dump_cache:
+    dump_cache()
